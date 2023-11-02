e801e8cc2fba738c521c7e4e4d55ad95910c69eb
Tardice <florent.bernar@outlook.fr>
2023-11-02 13:39:47 +0100

    Delete essai.py

--- Challenge/essai.py
+++
@@ -1,9 +0,0 @@
-number_un = 8
-number_deux = 5
-number_trois = 10
-
-
-if number_un < number_deux and number_deux < number_trois :
-    print("C'est croissant")
-else:
-    print("C'est pas croissant.")
